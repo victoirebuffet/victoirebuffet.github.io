7aa37a7450c9e9a18d408071e70b06c300fb4e36
victoirebuffet <154414896+victoirebuffet@users.noreply.github.com>
2025-02-11 17:50:11 +0000

    Deploying to gh-pages from @ victoirebuffet/victoirebuffet.github.io@0dbffabe2ce1313d053be1ad490bc73326a9cb9b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
